975b959c34ef6e6ab79b526ffd2d07c162548c7d
Giovanna <ggarciabas@gmail.com>
2020-09-20 11:13:09 -0300

    feat: model para finalidade

---
+++ models/finalidade.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const FinalidadeSchema = new Schema ({
+    descricao: {
+        type: String,
+        required: true,
+    },
+    fluxo: [{
+        type: String,
+        required: true
+    }],
+    entidade: {
+        type: Schema.Types.ObjectId,
+        ref: 'Entidade'
+    }
+}, {
+    timestamps: true,
+}); // createdAt, updatedAt - criado automático e atualizado pelo mongoose pela estrutura timestamp adicionada ao final
+
+module.exports = model('Finalidade', FinalidadeSchema);
