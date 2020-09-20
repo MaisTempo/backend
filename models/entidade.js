e54c12a9ae1b4b8d92ca5f8f18c81eb10ccc9a21
Giovanna <ggarciabas@gmail.com>
2020-09-20 11:10:14 -0300

    fix: nomenclatura

---
+++ models/entidade.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+const EntidadeSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+}); // createdAt, updatedAt - criado automático e atualizado pelo mongoose pela estrutura timestamp adicionada ao final
+
+module.exports = model('Entidade', EntidadeSchema);
