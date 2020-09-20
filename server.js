1625875b4a67a443572b259ec3f032d0e070f110
Giovanna <ggarciabas@gmail.com>
2020-09-20 00:15:17 -0300

    fix: conexão com atlas

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const devController = require('./controllers/empresa')
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require("mongoose");
 
 const api = express()
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 api.get('/', (req, res) => {
     return res.send('API para Projeto MaisTempo')
 })
 
+const devController = require('./controllers/empresa')
 api.get('/empresas', devController.index);
 
 api.listen(process.env.PORT || 8080)
